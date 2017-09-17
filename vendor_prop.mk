16b4226cbd7a7f40ecde4a31e39a35f2fcb584f7
Albert I <krascgq@outlook.co.id>
2017-09-17 12:47:52 +0800

    msm8996-common: dex2oat filter = speed
    Change-Id: Iee0f0f524490c813e43ff18fc5a327a119114aa0
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,6 +6,11 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
 
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
