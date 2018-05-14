be825b2ef5415fe4d5fb8801ac1d4301c784e57e
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    msm8996-common: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -108,8 +108,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 # Bluetooth
 PRODUCT_PACKAGES += \
