6b4332e660d83600ec076b1f0054088fbd2bb340
Offain <fabian.leutenegger@bluewin.ch>
2018-12-22 23:16:10 +0100

    msm8996-common: Disable EAP-SIM security
    Change-Id: Ia709147e4547f511419cd29868b2bcf6f6409ecc
    Signed-off-by: Aswin A S <aswinas7469@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,6 +143,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
+# EAP-SIM security
+DISABLE_EAP_PROXY := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
