f610cdc41a3e5650e79e013e4d3c14d078f882ed
Bladeyy <getsuga369@gmail.com>
2019-06-24 18:51:50 +0530

    msm8996-common: Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -194,6 +194,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.usb.rmnet.inst.name=rmnet \
     vendor.usb.rndis.func.name=rndis_bam
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
