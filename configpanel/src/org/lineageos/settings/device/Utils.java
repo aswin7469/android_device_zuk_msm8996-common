975d935c8e1c385056dd2dab86605002ad4993b2
Bruno Martins <bgcngm@gmail.com>
2019-09-01 13:05:37 +0100

    msm8996-common: ConfigPanel: Migrate to androidx
    Change-Id: Ia741c785c3ca1d6fb3d41ac68d64865731fba129

--- configpanel/src/org/lineageos/settings/device/Utils.java
+++ configpanel/src/org/lineageos/settings/device/Utils.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017 The LineageOS Project
+ *           (C) 2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,8 +25,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import android.content.SharedPreferences;
 import android.os.UserHandle;
-import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.PreferenceManager;
+
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 public class Utils {
 
