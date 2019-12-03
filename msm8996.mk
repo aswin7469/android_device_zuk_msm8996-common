9a0ff924c0a5a356d0588178f47bd990a1a3d3a3
aswin7469 <aswinas7469@gmail.com>
2019-12-03 07:11:20 +0530

    msm8996-common: overlay: fixup call record

--- msm8996.mk
+++ msm8996.mk
@@ -32,6 +32,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pixys
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
