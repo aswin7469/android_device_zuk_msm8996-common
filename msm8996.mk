830c6383769675bfb700cd6bb26c80f4a82d294b
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    msm8996-common: Use Pixel charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -442,3 +442,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# Charging
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
