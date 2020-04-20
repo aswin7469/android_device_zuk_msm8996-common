b559e3924b0ca3fecc74d4eefceaa742413d420f
aswin7469 <aswinas7469@gmail.com>
2020-04-20 06:56:46 +0000

    msm8996-common: remove system copy of  ims-ext-common
    This reverts commit ef30b61c5cb1cb9fd0dbb818160e2cededa5ab67.

--- msm8996.mk
+++ msm8996.mk
@@ -368,10 +368,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common_system \
-
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
