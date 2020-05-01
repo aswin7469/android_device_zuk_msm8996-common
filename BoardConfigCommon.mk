07284800820e9de337cb8f2b595b3f5673cf9c06
aswin7469 <aswinas7469@gmail.com>
2020-05-01 09:29:53 +0000

    Revert "msm8996-common: Dexpreopt everything"
    This reverts commit 78fdf3fcb0fba4c60643669810232acb465da44d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,11 +116,10 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT ?= true
-      endif
-    endif
+    WITH_DEXPREOPT ?= true
+  endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 TARGET_USES_C2D_COMPOSITION := true
