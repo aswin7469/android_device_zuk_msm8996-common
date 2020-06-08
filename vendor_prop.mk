6b5777c8b670348523637d80853febbdad196d62
tangrobin <tangrobin@google.com>
2020-06-08 23:11:12 +0800

    msm8996-common: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415
    Signed-off-by: Aswin A S <aswinas@pixysos.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -238,22 +238,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.wcg_composition_dataspace=143261696
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_egl_image_tracker=1 \
-    debug.sf.early_phase_offset_ns=11600000 \
-    debug.sf.early_app_phase_offset_ns=11600000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.latch_unsignaled=1 \
-    debug.sf.recomputecrop=0
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000 \
 
 # TimeService
 PRODUCT_PROPERTY_OVERRIDES += \
