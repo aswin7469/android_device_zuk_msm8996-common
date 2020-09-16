e7dbca5294ceb96f7c5896597735b62636a32dd6
aswin7469 <aswinas@pixysos.com>
2020-09-16 01:51:23 +0000

    msm8996-common: switch back to zui QMI/RIL/IMS
    * fixes ril and hopefully vowifi does work with zui blobs too
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- msm8996.mk
+++ msm8996.mk
@@ -346,7 +346,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
-    android.hardware.secure_element@1.0 \
     android.hardware.radio.config@1.0 \
     librmnetctl \
     libxml2
@@ -373,6 +372,7 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier
