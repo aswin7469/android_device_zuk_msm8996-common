f498e69e0b0fe1f7bec18ed14f3b2c9ab5f4881c
aswin7469 <aswinas@pixysos.com>
2020-09-22 15:25:51 +0000

    msm8996-common: bie bie hwui props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -122,20 +122,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1
 
-# HwUi
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=276 \
-    ro.hwui.layer_cache_size=144 \
-    ro.hwui.r_buffer_cache_size=24 \
-    ro.hwui.path_cache_size=96 \
-    ro.hwui.gradient_cache_size=3 \
-    ro.hwui.drop_shadow_cache_size=24 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
 # Higher fling velocities
 # for smoother scrolling and better responsiveness
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
