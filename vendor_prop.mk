0f47bce7a45147b32fbf96d04125eedebc660aa5
aswin7469 <aswinas@pixysos.com>
2020-09-26 08:41:40 +0000

    msm8996-common: vendor_prop: add missing wfd prop
    * got removed in 7736443

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -288,7 +288,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.enable_vds=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.video.disable.ubwc=1 \
-    persist.sys.wfd.nohdcp=1
+    persist.sys.wfd.nohdcp=1 \
+    debug.sf.enable_hwc_vds=1
 
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
