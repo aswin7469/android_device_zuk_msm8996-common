c49f46e56ab5dc3b281d7689bb6bf4daba650e6f
aswin7469 <aswinas@pixysos.com>
2020-11-06 12:30:01 +0530

    Revert "msm8996-common: Force triple frame buffers"
    This reverts commit 7523ac11036b16d3a380a6a7d493717bb308f546.
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -241,7 +241,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.wcg_composition_dataspace=143261696
