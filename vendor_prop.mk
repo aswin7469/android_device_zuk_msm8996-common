9d58139430dd4bbf36986b5abe3717268cc854ec
Yan Wang <yawanng@google.com>
2021-01-09 02:16:41 +0400

    msm8996-common: Enable readahead for iorapd
    Test: Run and check if readahead is enabled.
    Change-Id: I4c21e0d2e02d0cfaeb9c609c9f65cc35b5db7e15
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -168,9 +168,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.radio.calls.on.ims=1 \
     persist.vendor.vt.supported=1
 
-# Enable iorapd perfetto tracing for app starts
+# Iorapd
 PRODUCT_PROPERTY_OVERRIDES += \
-    iorapd.perfetto.enable=true
+    iorapd.perfetto.enable=true \
+    iorapd.readahead.enable=true
 
 # IWLAN
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
