098977a09c148fc00f219dd0bf1da13844157e67
aswin7469 <aswinas7469@gmail.com>
2021-02-01 04:52:07 +0000

    Revert "msm8996-common: switch back to zui QMI/RIL/IMS"
    This reverts commit e7dbca5294ceb96f7c5896597735b62636a32dd6.
    
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -348,6 +348,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
+    android.hardware.secure_element@1.0 \
     android.hardware.radio.config@1.0 \
     librmnetctl \
     libxml2
@@ -375,7 +376,6 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier
