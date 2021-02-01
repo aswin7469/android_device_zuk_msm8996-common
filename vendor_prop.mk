a22c7865090f4fb74bc70926c6cef6adcb2aef10
aswin7469 <aswinas@pixysos.com>
2021-02-01 16:25:08 +0530

    msm8996-common: cleanup unused ims/wfc properties
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -146,14 +146,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.VT_ENABLE=1 \
-    persist.vendor.radio.VT_HYBRID_ENABLE=1 \
-    persist.vendor.radio.videopause.mode=1 \
-    persist.qti.telephony.vt_cam_interface=2 \
-    persist.radio.VT_CAM_INTERFACE=2 \
-    persist.vendor.qti.telephony.vt_cam_interface=2 \
-    persist.vendor.radio.VT_CAM_INTERFACE=2 \
-    persist.vendor.vt.supported=1
+    service.qti.ims.enabled=1 \
+    persist.vendor.vt.supported=1 \
+    persist.radio.VT_HYBRID_ENABLE=1
 
 # Iorapd
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -163,10 +158,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # IWLAN
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.vendor.data.iwlan.enable=true \
-    persist.data.iwlan.enable=true \
-    persist.data.iwlan=1 \
-    persist.data.iwlan.ipsec.ap=1 \
-    persist.vendor.sys.cnd.iwlan=1 \
     ro.telephony.iwlan_operation_mode=legacy
 
 # Set lmkd options
