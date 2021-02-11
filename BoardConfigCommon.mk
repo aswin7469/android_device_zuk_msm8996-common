0f80c5575403db556049b486293c829f6f5ab6e7
aswin7469 <aswinas7469@gmail.com>
2021-02-11 05:30:38 +0000

    msm8996-common: stop including legacy seolicy
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,7 +164,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 VENDOR_SECURITY_PATCH := 2020-11-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
