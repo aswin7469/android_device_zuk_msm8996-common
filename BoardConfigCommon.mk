6b54932699b7fb01bbf40b46387540349b5282d3
aswin7469 <aswinas7469@gmail.com>
2021-02-13 19:16:24 +0000

    [DNM]msm8996-common: ignore neverallows
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,6 +165,7 @@ VENDOR_SECURITY_PATCH := 2020-11-05
 # SELinux
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_msm8996
